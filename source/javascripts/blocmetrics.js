88244339e4dcddb1a9834b4375c92a1be8efe85b
Karim Tarek <karimmtarek@gmail.com>
2015-01-22 00:38:46 -0500

    vanilla js trial

--- source/javascripts/blocmetrics.js
+++ source/javascripts/blocmetrics.js
@@ -3,7 +3,8 @@ var blocmetrics = {};
 //blocmetrics.track("eventName-123", [{key: "prop_1", value: "value_1"}, {key: "prop_2", value: "value_2"}])
 
 (function(){
-    blocmetrics.track = function(eventName, eventProperties){
+    blocmetrics.xtrack = function(eventName, eventProperties){
+        var api_url = "http://127.0.0.1:3000/events";
         var eventData = {
             event: {
                 name: eventName,
@@ -11,12 +12,30 @@ var blocmetrics = {};
             },
             properties: eventProperties
         };
+        var r = new XMLHttpRequest();
+        r.open("POST", api_url, true);
+        r.setRequestHeader('Content-Type', 'application/json');
+        r.onreadystatechange = function () {
+            // I'm not sure what should go in here?!
+        };
+        r.send(JSON.stringify(eventData));
 
+    };
+    //--------------------------------------------------------
+    blocmetrics.track = function(eventName, eventProperties){
+        var api_url = "http://127.0.0.1:3000/events";
+        var eventData = {
+            event: {
+                name: eventName,
+                source_url: window.location.hostname
+            },
+            properties: eventProperties
+        };
         $.ajax({
             method: 'POST',
-            url: 'http://127.0.0.1:3000/events',
+            url: api_url,
             data: eventData,
             error: function(e){console.log('Error: ' + e);}
         });
-    }
+    };
 }());
